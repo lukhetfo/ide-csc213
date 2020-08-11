6c1835be66f91d669f17e6284c28627932fa5287
lukhetfo <47589934+lukhetfo@users.noreply.github.com>
2020-08-11 13:21:16 +0200

    uploaded basic constructs code examples

---
+++ UNIT 02/SOURCE CODE/JAVA/BasicConstructsDemo_CLI.java
@@ -0,0 +1,81 @@
+
+package com.dube.csc213;
+
+/**
+ *
+ * @author Enock Lukhetfo Dube  <eldube@uniswa.sz>
+ */
+public class BasicConstructsDemo_CLI {
+
+    //Single Selection
+    static void singleSelectionExample(int x, int y) {
+	if (x > y) {
+		System.out.println(x);
+	}
+    }
+
+
+    //Double Selection
+    static void doubleSelectionExample(int x, int y) {
+	if (x > y) 
+		System.out.println(x);
+	else
+		System.out.println(y);
+    }
+
+    //Multiple Selection
+    static void multiplySelectionExample(int day) {
+	switch (day) {
+	case 6:
+	case 7:
+		System.out.println("Weekend");
+		break;
+	default:
+		System.out.println("Week Day");
+		break;
+	}
+    }
+
+    //While loop
+    static void whileLoopExample() {
+	int i = 1;
+	while (i < 5) {
+            System.out.print(i);
+            i++;
+	}
+
+	System.out.println();
+    }
+
+    //Do ... while loop
+    static void doWhileLoopExample() {
+	int i = 1;
+	do {
+		System.out.print(i);
+		i++;
+	} while (i < 5);
+
+	System.out.println();
+    }
+
+    //FOR loop
+    static void forLoopExample() {
+	for (int i = 1; i < 5; i++) {
+		System.out.print(i);
+	}
+
+	System.out.println();
+    }
+    public static void main(String[] args) {
+       //run selection code
+	singleSelectionExample(10, 5);
+	doubleSelectionExample(5, 10);
+	multiplySelectionExample(4);
+
+	//run iteration code
+	whileLoopExample();
+	doWhileLoopExample();
+	forLoopExample();
+    }
+    
+}
