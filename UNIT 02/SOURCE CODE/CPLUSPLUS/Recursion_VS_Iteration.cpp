ee53e3d610ed5488c3d2c13aa4a9254d17ca4fe8
lukhetfo <47589934+lukhetfo@users.noreply.github.com>
2020-08-24 13:07:44 +0200

    updated files

---
+++ UNIT 02/SOURCE CODE/CPLUSPLUS/Recursion_VS_Iteration.cpp
@@ -0,0 +1,31 @@
+// Recursion_VS_Iteration.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "pch.h"
+#include <iostream>
+
+
+int factorial(int n) {
+	if (n == 0) //base case/condition
+		return 1; 
+	else //recursive case
+		return n * factorial(n - 1); 
+}
+
+int factorial_Iterative(int n) {
+	int answer = 1; 
+	for (int i = 1; i <= n; i++) //iteration
+		answer = answer * i;
+	return answer;
+}
+
+
+int main()
+{
+	std::cout << factorial(5) << std::endl;
+	std::cout << factorial_Iterative(5) << std::endl;
+
+	system("pause");
+}
+
+
